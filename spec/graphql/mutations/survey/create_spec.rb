d228f6955016b501e9466f378db8b624b48dedaf
FranciscoLN <franciscocolatinodelima@gmail.com>
2024-01-01 12:17:50 -0300

    Alguns ajustes no teste de criação de pesquisa

--- spec/graphql/mutations/survey/create_spec.rb
+++ spec/graphql/mutations/survey/create_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe "creating a survey" do
   fixtures :users
   let(:user) { users(:test) }
 
+  deadline = (Date.today + 5.days).strftime('%Y-%m-%dT%H:%M:%SZ')
+
   it "created with success" do
-
-    deadline = (Date.today + 5.days).strftime('%Y-%m-%dT%H:%M:%SZ')
-
     result = SurveyApiSchema.execute(create_query, variables: {
       title: "Pesquisa teste 1",
       closed: false,
@@ -16,6 +15,16 @@ RSpec.describe "creating a survey" do
     expect(result.dig("data", "createSurvey", "survey", "title")).to eq("Pesquisa teste 1")
     expect(result.dig("data", "createSurvey", "survey", "closed")).to eq(false)
     expect(result.dig("data", "createSurvey", "survey", "deadline")).to eq(deadline)
+  end
+
+  it "returns an error when not authenticated" do
+    result = SurveyApiSchema.execute(create_query, variables: {
+      title: "Pesquisa teste 1",
+      closed: false,
+      deadline: deadline
+    })
+    expect(result.dig("data", "createSurvey")).to be_nil
+    expect(result["errors"].first["message"]).not_to be_blank
   end
 
   def create_query
